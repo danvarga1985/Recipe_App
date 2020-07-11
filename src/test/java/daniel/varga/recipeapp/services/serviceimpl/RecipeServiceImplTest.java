071a8563cb6b37ffffb76107ea5bcd48c948800e
danvarga1985 <danvarga1985@gmail.com>
2020-07-11 10:48:22 +0200

    Adding tests to RecipeController

--- src/test/java/daniel/varga/recipeapp/services/serviceimpl/RecipeServiceImplTest.java
+++ src/test/java/daniel/varga/recipeapp/services/serviceimpl/RecipeServiceImplTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,14 +23,13 @@ class RecipeServiceImplTest {
 
     @BeforeEach
     void setUp() {
+        //Same result as @InjectMocks on recipeService
         MockitoAnnotations.initMocks(this);
 
         recipeService = new RecipeServiceImpl(recipeRepository);
 
 
     }
-
-
 
     @Test
     void getRecipes() {
@@ -39,7 +39,7 @@ class RecipeServiceImplTest {
 
         recipesData.add(recipe);
 
-        when(recipeRepository.findAll()).thenReturn(recipesData);
+        when(recipeService.getRecipes()).thenReturn(recipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
@@ -47,5 +47,22 @@ class RecipeServiceImplTest {
 
         //Verifies that the recipeRepository.finall() method has been called only once.
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
+    }
+
+    @Test
+    void getRecipeByIdTest() {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull (recipeReturned,"Null recipe returned");
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
     }
 }
