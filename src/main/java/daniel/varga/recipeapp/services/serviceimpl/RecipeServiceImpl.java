91e061b0f8aba2a123e078e3a37e5e619385ca76
danvarga1985 <danvarga1985@gmail.com>
2020-07-14 16:27:26 +0200

    Implementing deleteRecipe service

--- src/main/java/daniel/varga/recipeapp/services/serviceimpl/RecipeServiceImpl.java
+++ src/main/java/daniel/varga/recipeapp/services/serviceimpl/RecipeServiceImpl.java
@@ -30,6 +30,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
+
         Set<Recipe> recipes = new HashSet<>();
 
         recipeRepository.findAll().iterator().forEachRemaining(recipes :: add);
