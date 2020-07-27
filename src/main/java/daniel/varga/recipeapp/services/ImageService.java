61b1d053c3ae4c8fa71e6cba8fee42ac003f9dc5
danvarga1985 <danvarga1985@gmail.com>
2020-07-27 11:10:14 +0200

    Showing Image on Recipe Form + Adding Converter tests

---
+++ src/main/java/daniel/varga/recipeapp/services/ImageService.java
@@ -0,0 +1,8 @@
+package daniel.varga.recipeapp.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
---
+++ src/main/java/daniel/varga/recipeapp/services/serviceimpl/ImageServiceImpl.java
@@ -0,0 +1,15 @@
+package daniel.varga.recipeapp.services.serviceimpl;
+
+import daniel.varga.recipeapp.services.ImageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService {
+    @Override
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+        log.debug("Received a file");
+    }
+}
