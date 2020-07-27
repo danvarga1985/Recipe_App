2dbde0dde192636689d41941adea1aa7b7778d15
danvarga1985 <danvarga1985@gmail.com>
2020-07-27 13:32:45 +0200

    Implementing Save Image

--- src/main/java/daniel/varga/recipeapp/services/serviceimpl/ImageServiceImpl.java
+++ src/main/java/daniel/varga/recipeapp/services/serviceimpl/ImageServiceImpl.java
@@ -1,15 +1,47 @@
 package daniel.varga.recipeapp.services.serviceimpl;
 
+import daniel.varga.recipeapp.domain.Recipe;
+import daniel.varga.recipeapp.repositories.RecipeRepository;
 import daniel.varga.recipeapp.services.ImageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received a file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //handle properly
+            log.error("Error happened", e);
+
+            e.printStackTrace();
+        }
     }
 }
